1d9f85eb7a757353258a19295b701e54e2c55eff
harshdulani <harshdulani@yahoo.in>
2022-01-17 14:37:44 +0530

    deleted rayfire for build, a lot of changes lost in poor git mgmt

---
+++ Assets/Scripts/Enemy/ShatterEnemyController.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class ShatterEnemyController : MonoBehaviour
+{
+	[SerializeField] private Transform climbing;
+	[SerializeField] private bool controlAnimator;
+	private RagdollController _ragdollController;
+	
+	private bool _hasReached;
+	
+	private Animator _anim;
+	private static readonly int Reached = Animator.StringToHash("reached");
+
+	private void OnEnable()
+	{
+		GameEvents.only.rayfireShattered += OnShatteredTower;
+	}
+	
+	private void OnDisable()
+	{
+		GameEvents.only.rayfireShattered -= OnShatteredTower;
+	}
+
+	private void Start()
+	{
+		if(controlAnimator)
+			_anim = GetComponent<Animator>();
+		_ragdollController = GetComponent<RagdollController>();
+		
+		if(!climbing)
+			Debug.LogWarning("climber not assigned to " + gameObject);
+	}
+	
+	public void ReachEnd()
+	{
+		if(controlAnimator)
+			_anim.SetTrigger(Reached);
+		_hasReached = true;
+	}
+
+	private void OnShatteredTower(Transform shattered)
+	{
+		if(shattered != climbing) return;
+		
+		_ragdollController.GoRagdoll(-transform.forward + transform.up * 2f);
+		GameEvents.only.InvokeEnemyKill();
+	}
+}
